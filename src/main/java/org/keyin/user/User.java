5c1d327b82839316af4b7b1a88142b9f2e40c091
Nick <174647283+npkav@users.noreply.github.com>
2025-04-01 22:54:23 -0230

    added constructor, getters, setters

--- src/main/java/org/keyin/user/User.java
+++ src/main/java/org/keyin/user/User.java
@@ -14,4 +14,94 @@ public class User {
     private String province;
     private String postalCode;
     private String role;
+
+
+    // constructor
+    public User(String username, String password, String email, String phone, String address, String city, String province, String postalCode, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+        this.role = role;
+    }
+
+
+    // getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }   
+    
+    public String getProvince() {
+        return province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    
+    // setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }   
+
+    public void setProvince(String province) {
+        this.province = province;
+    }   
+    
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
