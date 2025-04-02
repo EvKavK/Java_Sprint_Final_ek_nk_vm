467b421c22ae4957cff282143b672a9eab10fddb
EvKavK <evan.kavanagh@keyin.com>
2025-04-02 13:04:37 -0230

    Changed Database.sql to be in snake_case to help with CRUD development.

--- src/main/java/org/keyin/database/Database.sql
+++ src/main/java/org/keyin/database/Database.sql
@@ -1,28 +1,28 @@
 CREATE TABLE users (
-    userId SERIAL PRIMARY KEY,
-    userName TEXT,
-    userEmail TEXT,
-    userPhoneNumber TEXT,
-    userAddress TEXT,
-    userPassword TEXT,
-    userRole TEXT
+    user_id SERIAL PRIMARY KEY,
+    user_name TEXT,
+    user_email TEXT,
+    user_phoneNumber TEXT,
+    user_address TEXT,
+    user_password TEXT,
+    user_role TEXT
 );
 
 CREATE TABLE memberships (
-    membershipId SERIAL PRIMARY KEY,
-    membershipType TEXT,
-    membershipCost INT,
-    membershipStatus TEXT,
-    userId INT REFERENCES users(userId)
+    membership_id SERIAL PRIMARY KEY,
+    membership_type TEXT,
+    membership_cost INT,
+    membership_status TEXT,
+    member_id INT REFERENCES users(user_id)
 );
 
 CREATE TABLE workoutClasses (
-    workoutClassId SERIAL PRIMARY KEY,
-    workoutClassName TEXT,
-    trainerId REFERENCES users(userId)
+    workoutClass_id SERIAL PRIMARY KEY,
+    workoutClass_name TEXT,
+    trainer_id REFERENCES users(user_id)
 );
 
-INSERT INTO users (userName, userEmail, userPhoneNumber, userAddress, userPassword, userRole) 
+INSERT INTO users (user_name, user_email, user_phoneNumber, user_address, user_password, user_role) 
 VALUES 
 ('Jimbo Gym', 'thegymman@gmail.com', '1234567890', '123 Gym St.', 'testpassword1', 'Admin'),
 ('John Workout', 'johnworks@outlook.com', '0987654321', '456 Place Rd.', 'testpassword2', 'Trainer'),
@@ -30,14 +30,14 @@ VALUES
 ('Bobby Gains', 'bobbygains123@gmail.com', '2233445566', '987 Swole St.', 'testpassword4', 'Member'),
 ('Training Jane', 'janetrains@gmail.com', '3344556677', '654 Workout Dr.', 'testpassword5', 'Trainer');
 
-INSERT INTO memberships (membershipType, membershipCost, membershipStatus, userId) 
+INSERT INTO memberships (membership_type, membership_cost, membership_status, member_id) 
 VALUES 
 ('Standard', 50, 'Active', 2),
 ('Premium', 100, 'Active', 3),
 ('Standard', 50, 'Active', 4),
 ('Premium', 100, 'Inactive', 5);
 
-INSERT INTO workoutClasses (workoutClassName, trainerId) 
+INSERT INTO workoutClasses (workoutClass_name, trainer_id) 
 VALUES 
 ('Yoga for Beginners', 2),
 ('HIIT (High-Intensity Interval Training)', 2),
