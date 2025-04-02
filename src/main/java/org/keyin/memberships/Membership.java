fa8727c425b03d0c5c806745cf82437776ab41f1
Nick <174647283+npkav@users.noreply.github.com>
2025-04-02 10:48:07 -0230

    added fields to membership class, added membership constructor

--- src/main/java/org/keyin/memberships/Membership.java
+++ src/main/java/org/keyin/memberships/Membership.java
@@ -4,4 +4,21 @@ package org.keyin.memberships;
 // This is class file that represents a membership
 //
 public class Membership {
+    private int id;
+    private int userID;
+    private String type;
+    private String startDate;
+    private String endDate;
+    private String status;
+    private double price;
+
+    public Membership(int id, int userID, String type, String startDate, String endDate, String status, double price) {
+        this.id = id;
+        this.userID = userID;
+        this.type = type;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.price = price;
+    }   
 }
