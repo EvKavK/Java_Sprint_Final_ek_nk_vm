ee38b1917a5257f4037594bc75b9faffd6bf514d
Nick <174647283+npkav@users.noreply.github.com>
2025-04-01 23:02:29 -0230

    updated child classes

--- src/main/java/org/keyin/user/childclasses/Admin.java
+++ src/main/java/org/keyin/user/childclasses/Admin.java
@@ -3,4 +3,7 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Admin extends User {
+    public Admin(String username, String password, String email, String phone, String address, String city, String province, String postalCode, String role) {
+        super(username, password, email, phone, address, city, province, postalCode, role);
+    }
 }
--- src/main/java/org/keyin/user/childclasses/Member.java
+++ src/main/java/org/keyin/user/childclasses/Member.java
@@ -3,4 +3,7 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Member extends User {
+    public Member(String username, String password, String email, String phone, String address, String city, String province, String postalCode, String role) {
+        super(username, password, email, phone, address, city, province, postalCode, role);
+    }
 }
