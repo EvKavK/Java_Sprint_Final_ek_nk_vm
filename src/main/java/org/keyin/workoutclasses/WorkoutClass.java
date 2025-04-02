e0c27fe94fc3dcfabbb19cbaf12ee95acb0a15b9
Nick <174647283+npkav@users.noreply.github.com>
2025-04-02 13:14:34 -0230

    added fields to workoutclass

--- src/main/java/org/keyin/workoutclasses/WorkoutClass.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClass.java
@@ -1,4 +1,11 @@
 package org.keyin.workoutclasses;
 
 public class WorkoutClass {
+    private int id;
+    private String name;
+    private String description;
+    private String reps;
+    private String sets;
+    private String setDuration;
+    private String notes;
 }
